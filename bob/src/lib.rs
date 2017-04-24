83c899fcfd1d441f9002924e5feb9af788e5dcb5
Wassim <phoewass@users.noreply.github.com>
2017-04-24 02:17:45 +0100

    add bob exercice submission

---
+++ bob/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn reply(request: &str) -> &'static str{
+    let mut reply = "Whatever.";
+    println!("{}",t);
+    if request.is_empty() {
+        reply = "Fine. Be that way!"
+    } else if request.ends_with("?") {
+        reply = "Sure."
+    } else if request.split(|c:char| !c.is_alphabetic())
+                    .collect::<String>()
+                    .replace(char::is_uppercase,"")
+                    .is_empty()
+    {
+        reply = "Whoa, chill out!"
+    }
+    reply
+}
