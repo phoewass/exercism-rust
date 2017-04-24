588790da6b777aa09f34c32b2a74c5d0329e14d0
Wassim <phoewass@users.noreply.github.com>
2017-04-24 02:26:41 +0100

    improve submission using pattern matching

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,15 +1,8 @@
 pub fn reply(request: &str) -> &'static str{
-    let mut reply = "Whatever.";
-    if request.is_empty() {
-        reply = "Fine. Be that way!"
-    } else if request.ends_with("?") {
-        reply = "Sure."
-    } else if request.split(|c:char| !c.is_alphabetic())
-                    .collect::<String>()
-                    .replace(char::is_uppercase,"")
-                    .is_empty()
-    {
-        reply = "Whoa, chill out!"
+    match request {
+        "" => "Fine. Be that way!",
+        _ if request.ends_with('?') => "Sure.",
+        _ if request == request.to_uppercase() => "Whoa, chill out!",
+        _ => "Whatever.",
     }
-    reply
 }
